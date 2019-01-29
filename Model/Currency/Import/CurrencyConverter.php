e5cae61824ac9a30f704db8ef25a2be8eec60549
Clive Walkden <clive@sozodesign.co.uk>
2019-01-29 14:47:58 +0000

    Initial Commit

---
+++ Model/Currency/Import/CurrencyConverter.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Sozo\CurrencyConvertionExtended\Model\Currency\Import;
+
+/**
+ * Currency rate import model (From https://free.currencyconverterapi.com/)
+ */
+class CurrencyConverter extends \Magento\Directory\Model\Currency\Import\AbstractImport
+{
+    /**
+     * @var string
+     */
+    const CURRENCY_CONVERTER_URL = 'https://free.currencyconverterapi.com/api/v6/convert?q={{CURRENCY_FROM}}_{{CURRENCY_TO}}&compact=ultra';
+
+    /** @var \Magento\Framework\Json\Helper\Data */
+    protected $jsonHelper;
+
+    /**
+     * Http Client Factory
+     *
+     * @var \Magento\Framework\HTTP\ZendClientFactory
+     */
+    protected $httpClientFactory;
+
+    /**
+     * Core scope config
+     *
+     * @var \Magento\Framework\App\Config\ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * Initialize dependencies
+     *
+     * @param \Magento\Directory\Model\CurrencyFactory $currencyFactory
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Framework\HTTP\ZendClientFactory $httpClientFactory
+     * @param \Magento\Framework\Json\Helper\Data $jsonHelper
+     */
+    public function __construct(
+        \Magento\Directory\Model\CurrencyFactory $currencyFactory,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Framework\HTTP\ZendClientFactory $httpClientFactory,
+        \Magento\Framework\Json\Helper\Data $jsonHelper
+    ) {
+        parent::__construct($currencyFactory);
+        $this->scopeConfig = $scopeConfig;
+        $this->httpClientFactory = $httpClientFactory;
+        $this->jsonHelper = $jsonHelper;
+    }
+
+    /**
+     * @param string $currencyFrom
+     * @param string $currencyTo
+     * @param int $retry
+     * @return float|null
+     */
+    protected function _convert($currencyFrom, $currencyTo, $retry = 0)
+    {
+        $result = null;
+        $timeout = (int)$this->scopeConfig->getValue(
+            'currency/google/timeout',
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE
+        );
+
+        $key = $currencyFrom . '_' . $currencyTo;
+
+        $url = str_replace('{{CURRENCY_FROM}}', $currencyFrom, self::CURRENCY_CONVERTER_URL);
+        $url = str_replace('{{CURRENCY_TO}}', $currencyTo, $url);
+
+        /** @var \Magento\Framework\HTTP\ZendClient $httpClient */
+        $httpClient = $this->httpClientFactory->create();
+
+        try {
+            $response = $httpClient->setUri($url)
+                ->setConfig(['timeout' => $timeout])
+                ->request('GET')
+                ->getBody();
+
+            $data = $this->jsonHelper->jsonDecode($response);
+            if (isset($data[$key]['val'])) {
+                $result = (float)$data[$key]['val'];
+            } else {
+                $this->_messages[] = __('We can\'t retrieve a rate from %1.', $url);
+            }
+        } catch (\Exception $e) {
+            if ($retry == 0) {
+                $this->_convert($currencyFrom, $currencyTo, 1);
+            } else {
+                $this->_messages[] = __('We can\'t retrieve a rate from %1.', $url);
+            }
+        }
+        return $result;
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Currency Convertion Extended Magento 2 Plugin
+
+[![License](https://poser.pugx.org/sozodesign/magento2-currencyconvertionextended/license)](./LICENSE)
+[![Total Downloads](https://poser.pugx.org/sozodesign/magento2-currencyconvertionextended/downloads)](https://packagist.org/packages/sozodesign/magento2-currencyconvertionextended)
+[![Latest Stable Version](https://poser.pugx.org/sozodesign/magento2-currencyconvertionextended/v/stable)](https://packagist.org/packages/sozodesign/magento2-currencyconvertionextended)
+
+Adds additional currency converters to your Magento 2 site so that you are not forced to use fixer.io
+
+## Currency Systems Used
+ - [The Free Currency Converter API](https://free.currencyconverterapi.com/) 
+
+## Installation
+See the [installation.md](./Guides/INSTALLATION.md) guide
+
+## Usage
+see the [usage.md](./Guides/USAGE.MD) guide
